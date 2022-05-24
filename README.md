28e125afbbbbc3fb344115c4d5998022155c377c
ZawMinbliss <zawmin.thein.u20@bliss-stock.com>
2022-05-24 20:08:00 +0630

    added word source

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Authentication and Authorization in Node.js
 
-Authenticatate user using JWT and Authorization users API request with a custom claim base autorization middleware. 
-
-
 # Used stack
-
-Database: Mongodb with mongoose.  
-Backend Sever: Node.js with express.
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const dotenv = require("dotenv");
 
 const express = require("express");
 
-const mongoose = require("mongoose");
-
 const customerRoute = require("./src/routes/customer.route");
 
 const userRoute = require("./src/routes/user.route");
@@ -13,11 +11,6 @@ const app = express();
 //default middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//mongodb connection
-mongoose.connect("mongodb://localhost:27017/scrabble", () =>
-  console.log("mongoose connected")
-);
 
 app.use("/api/customer", customerRoute);
 
